fe9a6c7a76f82e0836f606b7942596564fbc6ea5
Ashish Shugani <ashishshugani.281@gmail.com>
2019-01-10 16:27:46 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
 console.log('May Node be with you');
 
 app.listen(process.env.PORT || 3000, function() {
@@ -10,20 +12,20 @@ app.listen(process.env.PORT || 3000, function() {
 var getIP = require('ipware')().get_ip;
 app.use(function(req, res, next) {
     var ipInfo = getIP(req);
-    console.log(ipInfo);
-    res.send(ipInfo);
+    //console.log(ipInfo);
+    //res.send(ipInfo);
     // { clientIp: '127.0.0.1', clientIpRoutable: false }
     next();
 });
 
 app.get('/', (req, res) => {
-  	res.send('Hello World')
+  	//res.send('Hello World')
   	//const { headers, method, url } = request;
-  	res.send(ipInfo);
-  	console.log(req.method);
- 	console.log(req.ip);
-	console.log(req.connection.remoteAddress);
-
+  	//res.send(ipInfo);
+  	//console.log(req.method);
+ 	//console.log(req.ip);
+	//console.log(req.connection.remoteAddress);
+	res.redirect("image.html");
 
 })
 
